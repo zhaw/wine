4fc5855f2aed8d367aac2307278043b6ab89164f
ZhaoW <zhaow@Server.(none)>
2015-09-29 15:50:51 +0800

    file brand map

--- bof/show.m
+++ bof/show.m
@@ -1,5 +1,12 @@
+a = load('../file_brand_map.mat');
+map = a.map;
+right = 0;
 for j = 1:1500
     result{j,1}
+    if map(result{j,1}) == map(result{j,2})
+        right = right+1;
+        continue
+    end
     subplot(3,5,1)
     im = imread(['pics/test2_cutted/' result{j,1} '.png']);
     imshow(im);
@@ -10,3 +17,4 @@ for j = 1:1500
     end
     pause
 end
+right
