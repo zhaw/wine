f4ab2a523167d6773349979d02c6893c2ff9bd6c
ZhaoW <zhaow@Server.(none)>
2015-09-29 16:22:01 +0800

    auto judge

--- bof/show.m
+++ bof/show.m
@@ -1,17 +1,21 @@
 a = load('../file_brand_map.mat');
 map = a.map;
+result = load('result');
+result = result.result;
 right = 0;
 for j = 1:1500
     result{j,1}
-    if map(result{j,1}) == map(result{j,2})
+    map(result{j,1})
+    map(result{j,2})
+    if strcmp(map(result{j,1}), map(result{j,2}))
         right = right+1;
         continue
     end
-    subplot(3,5,1)
+    subplot(1,2,1)
     im = imread(['pics/test2_cutted/' result{j,1} '.png']);
     imshow(im);
-    for k = 1:14
-        subplot(3,5,k+1)
+    for k = 1:1
+        subplot(1,2,k+1)
         im = imread(['pics/train/' result{j,k+1} '.jpg']);
         imshow(im);
     end
